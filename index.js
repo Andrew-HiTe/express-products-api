ec74ecc946572f58b4a1adcd5389f40d83b45eef
Laboratório FIAP <labsfiap@fiap.com.br>
2025-07-02 11:00:15 -0300

    Teste Trigger

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get('/produtos', (req, res) => {
     { id: 2, nome: 'Mouse', preco: 150 },
     { id: 3, nome: 'Teclado Mecânico', preco: 500 },
     { id: 4, nome: 'Livro: DevSecOps', preco: 1200 }
+    { id: 4, nome: 'Livro: Aula na Fiap', preco: 10 }
   ]);
 });
 
